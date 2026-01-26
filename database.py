c5841c5c4c8340d2ab63189625d915a19d3f0e61
Alexey Zhilin <zhilin03@list.ru>
2026-01-26 21:48:09 +0200

    Убрано пересоздание базы

--- database.py
+++ database.py
@@ -46,7 +46,6 @@ class User(Base):
 
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
         print("База данных PostgreSQL инициализирована и таблицы созданы")
 
